eca5ea4aa1d2f3c2c74a9cbd25ce12052e7b2fc0
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-04-11 23:21:05 -0300

    Programa inicializado.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Chive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("\nBem vindo!");
+            Console.ResetColor();
+            Console.WriteLine("\n\nPoderia por favor digitar um bom trava lingua com (r),em seguida vou tentar repetir kkkk");
+            Console.WriteLine("\nNesse caso vamos ver quem é o melhor!");
+            Console.ReadLine();
+            {
+                
+            }
+        }
+    }
+}
